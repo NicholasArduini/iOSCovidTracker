9bb6a2d9b3fac1fd3ff432b50d3024dccb37fcf6
Nicholas Arduini <arduininicholas@gmail.com>
2020-06-14 13:49:05 -0400

    Initial commit

---
+++ Covid-19 Tracker/SummaryView.swift
@@ -0,0 +1,21 @@
+//
+//  SummaryView.swift
+//  Covid-19 Tracker
+//
+//  Created by Nicholas Arduini on 2020-06-14.
+//  Copyright © 2020 Nicholas Arduini. All rights reserved.
+//
+
+import SwiftUI
+
+struct SummaryView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct SummaryView_Previews: PreviewProvider {
+    static var previews: some View {
+        SummaryView()
+    }
+}
---
+++ Covid-19 Tracker/Views/CountrySummary/SummaryViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  SummaryViewModel.swift
+//  Covid-19 Tracker
+//
+//  Created by Nicholas Arduini on 2020-06-14.
+//  Copyright © 2020 Nicholas Arduini. All rights reserved.
+//
+
+import Foundation
